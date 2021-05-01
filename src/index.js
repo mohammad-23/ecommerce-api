6bc364edb7ef74e507ff686a2fa5443657d8123e
mohammad-23 <mohammad8758@hotmail.com>
2021-05-01 17:43:59 +0530

    Fix mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection
+  .once("open", () => console.log("Connected to MongoLab instance."))
+  .on("error", (error) => console.log("Error connecting to MongoLab:", error));
 
 const app = express();
 
